0bd8a5b892d389ea69dab3f26e9183ecf4759d4f
Amanuel <amaniye1@live.com>
2017-04-16 17:47:45 -0400

    Added news articles function

--- server.js
+++ server.js
@@ -41,4 +41,20 @@ app.get('/news', function (request, response) {
   })
 })
 
+// function to create articlesfunction createArticle (article) {
+function createArticle (article) {
+  var id = Object.keys(articles).length
+  article.createdAt = new Date()
+  articles[id] = article
+}
+
+createArticle({
+  title: 'Season 5 Premeires on April 4.',
+  content: 'Filming on the new episodes was scheduled for the spring of 2016 once Purcell and Miller had wrapped on the first season of Legends of Tomorrow - a new spin-off from The Flash in which the pair were also series regulars (busy boys!). Prison Break season 5 will launch on Fox in the US. The premiere date? Tuesday, April 4, 2017, as part of the network\'s midseason schedule. Lincoln and Sara work together to track down Michael after certain evidence suggests that he could still be alive, which takes Lincoln over to Yemen. read more at '
+})
+createArticle({
+  title: 'Prison Break is back on our screens years after the original came to an end',
+  content: 'The new series begins with Miller acknowledging this: "I died seven years ago," he says before going on to talk about freedom then the all important discovery by his brother Lincoln, that Michael may actually be alive and in Yemen prison. Read more at '
+})
+
 app.listen(port)
