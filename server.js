49d2b8b0f252e38aff9197c25dc5cd6bd994c51a
Amanuel <amaniye1@live.com>
2017-04-18 18:15:01 -0400

    Fixed minor errors

--- server.js
+++ server.js
@@ -3,9 +3,12 @@ var express = require('express')
 var app = express()
 var port = process.env.PORT || 8080
 var articles = {
-  title: '',
-  content: ''
 }
+
+var photo1 = '/img/pb-flash.png'
+var photo2 = '/img/prison-break1.jpg'
+var link1 = "https://en.wikipedia.org"
+var link2 = "https://en.wikipedia.org/wiki/Prison_Break'"
 
 // define the templating engine as ejs
 app.set('view engine', 'ejs')
@@ -47,11 +50,16 @@ function createArticle (article) {
 
 createArticle({
   title: 'Season 5 Premeires on April 4.',
-  content: 'Filming on the new episodes was scheduled for the spring of 2016 once Purcell and Miller had wrapped on the first season of Legends of Tomorrow - a new spin-off from The Flash in which the pair were also series regulars (busy boys!). Prison Break season 5 will launch on Fox in the US. The premiere date? Tuesday, April 4, 2017, as part of the network\'s midseason schedule. Lincoln and Sara work together to track down Michael after certain evidence suggests that he could still be alive, which takes Lincoln over to Yemen. read more at '
+  image: photo1,
+  content: 'Filming on the new episodes was scheduled for the spring of 2016 once Purcell and Miller had wrapped on the first season of Legends of Tomorrow - a new spin-off from The Flash in which the pair were also series regulars (busy boys!). Prison Break season 5 will launch on Fox in the US. The premiere date? Tuesday, April 4, 2017, as part of the network\'s midseason schedule. Lincoln and Sara work together to track down Michael after certain evidence suggests that he could still be alive, which takes Lincoln over to Yemen. read more at ',
+  link: link1
 })
+
 createArticle({
   title: 'Prison Break is back on our screens years after the original came to an end',
-  content: 'The new series begins with Miller acknowledging this: "I died seven years ago," he says before going on to talk about freedom then the all important discovery by his brother Lincoln, that Michael may actually be alive and in Yemen prison. Read more at '
+  image: photo2,
+  content: 'The new series begins with Miller acknowledging this: "I died seven years ago," he says before going on to talk about freedom then the all important discovery by his brother Lincoln, that Michael may actually be alive and in Yemen prison. Read more at ',
+  link: link2
 })
 
 app.listen(port)
